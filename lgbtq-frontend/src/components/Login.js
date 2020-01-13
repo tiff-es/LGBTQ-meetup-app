ec41e1ee437c677196f8a675e5ad9c9038ac438a
boostinwrx <tony.calablava@gmail.com>
2020-01-13 11:33:00 -0500

    Login Screen added buttons login and create account (not wired up yet)

--- lgbtq-frontend/src/components/Login.js
+++ lgbtq-frontend/src/components/Login.js
@@ -1,11 +1,16 @@
 import React from "react";
-import {Toolbar} from "@material-ui/core";
+import {Toolbar, Button, GridList} from "@material-ui/core";
 import {NavLink, Route, BrowserRouter} from "react-router-dom";
 import Home from "./Home";
+
 export default class Login extends React.Component{
     render() {
   return(
-      <div>'Hello'</div>
-  )
+      <div>
+          <GridList>
+      <Button>Log in</Button>
+      <Button>Create Account</Button>
+          </GridList>
+      </div>)
     }
 }
