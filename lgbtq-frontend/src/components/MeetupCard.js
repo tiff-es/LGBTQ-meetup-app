152b9b7320cbd5fd1d4be3ef4b959d5767672870
boostinwrx <tony.calablava@gmail.com>
2020-02-04 01:13:46 -0500

    Changed DB, added Moment.js, meetup cards working, needs category and location w/ mapbox

--- lgbtq-frontend/src/components/MeetupCard.js
+++ lgbtq-frontend/src/components/MeetupCard.js
@@ -5,11 +5,15 @@ const MeetupCard = (props) => {
 
     return(
 
-        <Card>
-            <Card.Title className='title'><b>{props.name}</b></Card.Title>
-            <Card.Header className='header'>Info: <b>{props.info}</b> </Card.Header>
-            <Card.Body className='body'>Start Time: <b><Moment date={props.time} format='hh:mm A'> </Moment></b></Card.Body>
-            <Card.Body className='body'>Date: <b><Moment format='ddd MMM DD, YYYY'>{props.date}</Moment></b></Card.Body>
+        <Card className='card'>
+            <Card.Title className='header'><h3><b>{props.name}</b></h3></Card.Title>
+            <Card.Subtitle className='header'><h6><b>{props.info}</b> </h6></Card.Subtitle>
+            <Card.Body className='header'> Location: <b>{props.location}</b></Card.Body>
+            <Card.Body className='body'>Start Time: <b><Moment date={props.time} format='hh:mm A'> </Moment></b>
+                <br/>Date: <b><Moment format='ddd MMM DD, YYYY'>{props.date}</Moment></b>
+
+            </Card.Body>
+
         </Card>
 
     )
