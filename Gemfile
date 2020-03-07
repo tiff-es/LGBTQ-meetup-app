dc3fa4dda4db454b5f1200ea977bf15a25ccdaa6
boostinwrx <tony.calablava@gmail.com>
2020-03-07 18:56:20 -0500

    Heroku backend init

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
+gem 'activerecord-jdbcpostgresql-adapter'
 
 group :development do
   gem 'listen', '>= 3.0.5', '< 3.2'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   has_many :users_meetups
   has_many :meetups, through: :users_meetups
   validates :username, presence: true
-  validates :password, presence: true
   validates :name, presence: true
   validates :picture, presence: true
   validates :bio, presence: true
