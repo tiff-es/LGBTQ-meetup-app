22769ccc304511b687683ea05ca597a692515927
boostinwrx <tony.calablava@gmail.com>
2020-03-07 04:39:12 -0500

    Login/log out feture begun tweaking. Log out function is working on navbar but not updating upon event

--- lgbtq-frontend/src/components/NavBar.js
+++ lgbtq-frontend/src/components/NavBar.js
@@ -9,9 +9,26 @@ import '../assets/css/navbar.css'
 import {NavbarBrand, NavDropdown} from "react-bootstrap";
 import NavbarCollapse from "react-bootstrap/NavbarCollapse";
 import connect from "react-redux/lib/connect/connect";
+import {clearMessage, userLoginFetch} from "../actions/user";
 export default class NavBar extends Component {
+state = {
+    token: null
+}
+componentDidMount() {
+    this.tokenTest()
+}
+    tokenTest(){
+      const  token = localStorage.getItem('token')
+        {token ? this.setState({token: true}) :  this.setState({token: false})}
+    }
 
     render(){
+        const login = () => {  return(              <NavLink to="/login"><Button id='logIn'className='navbar-btn'><div className='nav-link'>Login</div></Button></NavLink>
+        )}
+
+        const logOut = () => {  return(              <a><Button onClick={localStorage.removeItem('token') && this.props.history.push('/loginrequired')} className='navbar-btn'><div className='nav-link'>Log Out</div></Button></a>
+        )}
+
 
         return(
             <Navbar id='navbar' className='nav-flex-icons default-color'>
@@ -24,11 +41,13 @@ export default class NavBar extends Component {
                 {this.loggedIn}
                 <NavLink to="/"><Button className='nav-item'><div className='nav-link'>Home</div></Button></NavLink>
                 <NavLink to="/createaccount"><Button className='navbar-btn'><div className='nav-link'>Create Account</div></Button></NavLink>
-                <NavLink to="/login"><Button className='navbar-btn'><div className='nav-link'>Login</div></Button></NavLink>
+                { login()  }
                 <NavLink to="/meetups"><Button className='navbar-btn'><div className='nav-link'>Meetups</div></Button></NavLink>
 
                 <NavLink to="/createmeetup"><Button className='navbar-btn'><div className='nav-link'>Create Meetup</div></Button></NavLink>
                 {/*<NavLink to='/myprofile'><Button className='navbar-btn'><div className='nav-link'>My Profile </div> </Button> </NavLink>*/}
+                {this.state.token ? logOut() : ''}
+
             </Navbar>
 
 
@@ -42,4 +61,6 @@ const mapStateToProps = (state) => {
         isLoggedIn: state.users.authenticated
     }
 }
+
+
 connect (mapStateToProps)(Navbar)
