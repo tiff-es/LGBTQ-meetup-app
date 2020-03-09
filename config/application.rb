f9cc7274a8ae53a9639bfa73eb6b4c4d5fc52b9e
boostinwrx <tony.calablava@gmail.com>
2020-03-08 23:59:09 -0400

    Cors preflight method test

--- config/application.rb
+++ config/application.rb
@@ -23,10 +23,11 @@ module LGBTQBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => :any # also tried [:get, :post, etc]
       end
     end
     # Settings in config/environments/* take precedence over those specified here.
